0d27d90d308ecc2e71d515bfd3a284912ac3b865
Joel <joel.v.lehtonen@gmail.com>
2026-02-04 21:05:28 +0200

    started prototyping services

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,49 @@
 import './index.css'
 import { Button } from './components/Button'
 import { Input } from './components/Input'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { fetchVenueStaticData } from './services/fetchVenueStaticData'
 
 function App() {
-    const [venue, setVenue] = useState('Select your venue')
-    const [cartValue, setCartValue] = useState('Insert cart value')
-    const [latitude, setLatitude] = useState('Insert your latitude')
-    const [longitude, setLongitude] = useState('Insert your longitude')
+    const [venue, setVenue] = useState('Select your venue');
+    const [cartValue, setCartValue] = useState('Insert cart value');
+    const [latitude, setLatitude] = useState('Insert your latitude');
+    const [longitude, setLongitude] = useState('Insert your longitude');
+    const [availableVenues, setAvailableVenues] = useState([])
+    const [loading, setLoading] = useState(false);
+    const slugs = [
+        'home-assignment-venue-helsinki',
+        'home-assignment-venue-tallinn'
+    ]
+
+    useEffect(() => {
+        fetchVenueStaticData(slugs)
+        .then(set)
+
+    }, [])
+
+    const handleClick = (event: React.ChangeEvent<HTMLInputElement>) => {
+        console.log('CLICK');
+
+        if (event.target.value === event.target.defaultValue) {
+            event.target.value = '';
+        }
+    }
 
     const handleInput = (event: React.ChangeEvent<HTMLInputElement>, field: string) => {
         switch (field) {
             case 'venue':
-                setVenue(event.target.value)
-                break
+                setVenue(event.target.value);
+                break;
             case 'cart':
-                setCartValue(event.target.value)
-                break
+                setCartValue(event.target.value);
+                break;
             case 'latitude':
-                setLatitude(event.target.value)
-                break
+                setLatitude(event.target.value);
+                break;
             case 'longitude':
-                setLongitude(event.target.value)
-                break
+                setLongitude(event.target.value);
+                break;
         }
     }
 
@@ -30,10 +51,10 @@ function App() {
         <div className='min-h-screen min-w-screen bg-[#00C2E8]'>
             <h1 className='text-white flex justify-center p-5'>Delivery Order Price Calculator</h1>
             <div className='grid grid-cols-5 gap-1'>
-                <Input label='venueSlug' text='Venue' value={venue} pattern='[A-za-z]' onChange={(e) => handleInput(e, 'venue')}/>
-                <Input label='cartValue' text='Cart value' value={cartValue} pattern='[A-za-z0-9]' onChange={(e) => handleInput(e, 'cart')}/>
-                <Input label='userLatitude' text='Latitude' value={latitude} pattern='[A-za-z0-9]' onChange={(e) => handleInput(e, 'latitude')}/>
-                <Input label='userLongitude' text='Longitude' value={longitude} pattern='[A-za-z0-9]' onChange={(e) => handleInput(e, 'longitude')}/>
+                <Input label='venueSlug' text='Venue' value={venue} pattern='^[A-Za-z]+$' onChange={(e) => handleInput(e, 'venue')} onClick={handleClick} />
+                <Input label='cartValue' text='Cart value' value={cartValue} pattern='^[A-Za-z0-9]+$' onChange={(e) => handleInput(e, 'cart')} onClick={handleClick} />
+                <Input label='userLatitude' text='Latitude' value={latitude} pattern='^[A-Za-z0-9]+$' onChange={(e) => handleInput(e, 'latitude')} onClick={handleClick} />
+                <Input label='userLongitude' text='Longitude' value={longitude} pattern='^[A-Za-z0-9]+$' onChange={(e) => handleInput(e, 'longitude')} onClick={handleClick} />
                 <Button text='Get location' />
                 <Button text='Calculate delivery price' />
             </div>
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -4,13 +4,14 @@ type InputProps = {
     value: string;
     pattern: string;
     onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    onClick: () => void;
 }
 
-export const Input = ({ label, text, value, pattern, onChange }: InputProps) => {
+export const Input = ({ label, text, value, pattern, onChange, onClick }: InputProps) => {
     return (
         <>
             <label htmlFor={label} className='flex col-start-2 items-center justify-end-safe text-white'>{text}</label>
-            <input id={label} type='text' value={value} pattern={pattern} onChange={onChange}
+            <input id={label} type='text' value={value} pattern={pattern} onChange={onChange} onClick={onClick}
                 className='col-start-3 border-1 border-black rounded m-1 pl-1 text-gray-700 bg-white' />
         </>
     )
